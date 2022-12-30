00267a40ba6cef785954bc5529d40b15489a5e14
Newbiecodeweaver <carlosgilino@gmail.com>
2022-12-30 10:51:28 +0100

    Rover class defined

--- src/rover.php
+++ src/rover.php
@@ -1,5 +1,25 @@
 <?php
 
-class Rover {
+namespace Rover;
 
-}
+require("../vendor/autoload.php");
+
+    class Rover {
+
+        public $name = "Curiosity";
+        public $location = array();
+        public $direction;
+        public $isStopped;
+        public $response;
+
+
+        public function __construct( $name, $location, $direction )
+        {
+            $this->name = $name;
+            $this->location = array( 'x' => $location['x'], 'y' => $location['y'] );
+            $this->direction = $direction;
+
+        }
+
+        
+    }

9815dfb18c2465f050a971b645e49c3219916884
Newbiecodeweaver <carlosgilino@gmail.com>
2022-12-30 12:47:03 +0100

    OnlyAllowedCommandsAreSent test completed

---
+++ tests/EarthTest.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+
+use Earth\Earth;
+use PHPUnit\Framework\TestCase;
+use Rover\Rover;
+
+require("../vendor/autoload.php");
+
+final class EarthTest extends TestCase
+{
+    public function testOnlyAllowedCommandsAreSent(): void
+    {
+        $newEarth = new Earth();
+    
+        $this->assertInstanceOf( Earth::class, $newEarth );
+
+        $response = $newEarth->sendCommandRover( "FLR" );
+
+        $this->assertStringStartsWith(" Rover location is", $response );
+
+    }
+}
+
+
+
+

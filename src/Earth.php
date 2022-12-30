372e17b7a23562bd8bd76f98bd4d1f07bbf6ca7b
Newbiecodeweaver <carlosgilino@gmail.com>
2022-12-30 10:34:42 +0100

    Earth class defined

---
+++ src/Earth.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Earth;
+
+use Planet\Planet;
+use Rover\Rover;
+
+require("../vendor/autoload.php");
+
+    class Earth extends Planet {
+
+        private $name = "Earth";
+
+    }

e57479febdddcbfc9ad9001de3cbf79f95c09139
Newbiecodeweaver <carlosgilino@gmail.com>
2023-01-04 11:43:45 +0100

    F_Command_And_N_Direction_Results_In_Y_Plus_One test defined

--- tests/RoverTest.php
+++ tests/RoverTest.php
@@ -4,18 +4,35 @@ use PHPUnit\Framework\TestCase;
 use Faker\Factory;
 use Rover\Rover;
 
-require("../vendor/autoload.php");
-
 final class RoverTest extends TestCase
 {
 
-    public function testRoverClassCanBeInstantiable(): void
+    protected $rover;
+    protected $location;
+    protected $direction;
+
+    protected function setUp(): void
     {
         $faker = Factory::create();
-        $roverTest = new Rover( $faker->text(5), $location = array( 'x' => $faker->numberBetween(0, 25), 'y' => $faker->numberBetween(0, 25) ), $faker->randomElement( ['F', 'L', 'R'] ) );
-
-        $this->assertInstanceOf( Rover::class, $roverTest );
+        $this->rover = new Rover( $faker->text(5), $location = array( 
+            'x' => $faker->numberBetween(0, 3), 
+            'y' => $faker->numberBetween(0, 5) ),
+            null );
     }
+
+
+    public function test_F_Command_And_N_Direction_Results_In_Y_Plus_One() : void
+    {
+
+        $initialLocation = $this->rover->location;
+
+        $this->rover->direction = "N";
+        $this->rover->moveRover( "F" );
+
+        $this->assertSame( $this->rover->location['y'], $initialLocation['y'] +1);
+
+    }
+
 }
 
 

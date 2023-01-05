02994f373e2fc4e54df8423ecd35404fa4f5f412
Newbiecodeweaver <carlosgilino@gmail.com>
2023-01-05 22:14:46 +0100

    new unit tests to control Rover bahave with obstacles

--- tests/RoverTest.php
+++ tests/RoverTest.php
@@ -2,6 +2,7 @@
 
 use PHPUnit\Framework\TestCase;
 use Faker\Factory;
+use Mars\Mars;
 use Rover\Rover;
 
 final class RoverTest extends TestCase
@@ -10,14 +11,18 @@ final class RoverTest extends TestCase
     protected $rover;
     protected $location;
     protected $direction;
+    protected $mars;
+    protected $faker;
 
     protected function setUp(): void
     {
-        $faker = Factory::create();
-        $this->rover = new Rover( $faker->text(5), $location = array( 
-            'x' => $faker->numberBetween(5, 10), 
-            'y' => $faker->numberBetween(5, 10) ),
+        $this->faker = Factory::create();
+        $this->rover = new Rover( $this->faker->text(5), $location = array( 
+            'x' => $this->faker->numberBetween(5, 10), 
+            'y' => $this->faker->numberBetween(5, 10) ),
             null );
+
+        $this->mars = new Mars();
     }
 
 
@@ -33,6 +38,7 @@ final class RoverTest extends TestCase
 
     }
 
+
     public function test_L_Command_And_N_Direction_Results_In_X_Minus_One() : void
     {
 
@@ -44,6 +50,7 @@ final class RoverTest extends TestCase
         $this->assertSame( $this->rover->location['x'], $initialLocation['x'] -1);
 
     }
+
 
     public function test_R_Command_And_N_Direction_Results_In_X_Plus_One() : void
     {
@@ -59,7 +66,6 @@ final class RoverTest extends TestCase
     }
 
 
-
     public function test_F_Command_And_S_Direction_Results_In_Y_Minus_One() : void
     {
 
@@ -72,6 +78,7 @@ final class RoverTest extends TestCase
 
     }
 
+
     public function test_L_Command_And_S_Direction_Results_In_X_Minus_One() : void
     {
 
@@ -83,6 +90,7 @@ final class RoverTest extends TestCase
         $this->assertSame( $this->rover->location['x'], $initialLocation['x'] +1);
 
     }
+
 
     public function test_R_Command_And_S_Direction_Results_In_X_Plus_One() : void
     {
@@ -110,6 +118,7 @@ final class RoverTest extends TestCase
 
     }
 
+
     public function test_L_Command_And_E_Direction_Results_In_Y_Plus_One() : void
     {
 
@@ -121,6 +130,7 @@ final class RoverTest extends TestCase
         $this->assertSame( $this->rover->location['y'], $initialLocation['y'] +1);
 
     }
+
 
     public function test_R_Command_And_E_Direction_Results_In_Y_Minus_One() : void
     {
@@ -136,7 +146,6 @@ final class RoverTest extends TestCase
     }
 
 
-    
     public function test_F_Command_And_W_Direction_Results_In_X_Minus_One() : void
     {
 
@@ -149,6 +158,7 @@ final class RoverTest extends TestCase
 
     }
 
+
     public function test_L_Command_And_W_Direction_Results_In_Y_Minus_One() : void
     {
 
@@ -160,6 +170,7 @@ final class RoverTest extends TestCase
         $this->assertSame( $this->rover->location['y'], $initialLocation['y'] -1);
 
     }
+
 
     public function test_R_Command_And_W_Direction_Results_In_Y_Plus_One() : void
     {
@@ -175,8 +186,143 @@ final class RoverTest extends TestCase
     }
 
 
+    public function test_Rover_Do_Not_Moves_To_Location_When_Finds_Obstacle_At_X_Plus_One() : void
+    {
+        $this->rover->location = array( 
+            'y' => $this->mars->obstacle['y'] , 
+            'x' => $this->mars->obstacle['x'] -1);
+
+        $this->rover->detectMarsObstacle( array(
+            'y' => $this->mars->obstacle['y'] , 
+            'x' => $this->mars->obstacle['x'] ));
+
+        $this->assertNotSame( $this->rover->location['x'], $this->mars->obstacle['x'] );
+
+
+    }
+
+
+    public function test_Rover_Do_Not_Moves_To_Location_When_Finds_Obstacle_At_X_Minus_One() : void
+    {
+        $this->rover->location = array( 
+            'y' => $this->mars->obstacle['y'] , 
+            'x' => $this->mars->obstacle['x'] +1);
+
+        $this->rover->detectMarsObstacle( array(
+            'y' => $this->mars->obstacle['y'] , 
+            'x' => $this->mars->obstacle['x'] ));
+
+        $this->assertNotSame( $this->rover->location['x'], $this->mars->obstacle['x'] );
+
+
+    }
+
+
+    public function test_Rover_Confirm_Location_When_Does_Not_Find_Obstacles_At_X_Plus_One() : void
+    {
+
+         $initialLocation = $this->rover->location;
+
+         $newLocation = array( 
+            'y' => $this->rover->location['y'], 
+            'x' => $this->rover->location['x'] + 1 );
+
+        $this->rover->detectMarsObstacle( array(
+            'y' => $this->mars->obstacle['y'] , 
+            'x' => $this->mars->obstacle['x'] ));
+
+        $this->assertSame( $newLocation['x'], $initialLocation['x'] + 1 );
+
+
+    }
+
+
+    public function test_Rover_Confirm_Location_When_Does_Not_Find_Obstacles_At_X_Minus_One() : void
+    {
+
+         $initialLocation = $this->rover->location;
+
+         $newLocation = array( 
+            'y' => $this->rover->location['y'], 
+            'x' => $this->rover->location['x'] -1 );
+
+        $this->rover->detectMarsObstacle( array(
+            'y' => $this->mars->obstacle['y'] , 
+            'x' => $this->mars->obstacle['x'] ));
+
+        $this->assertSame( $newLocation['x'], $initialLocation['x'] - 1 );
+
+
+    }
+
+
+    public function test_Rover_Do_Not_Moves_To_Location_When_Finds_Obstacle_At_Y_Plus_One() : void
+    {
+        $this->rover->location = array( 
+            'y' => $this->mars->obstacle['y'] -1 , 
+            'x' => $this->mars->obstacle['x']);
+
+        $this->rover->detectMarsObstacle( array(
+            'y' => $this->mars->obstacle['y'] , 
+            'x' => $this->mars->obstacle['x'] ));
+
+        $this->assertNotSame( $this->rover->location['y'], $this->mars->obstacle['y'] );
+
+
+    }
+
+
+    public function test_Rover_Do_Not_Moves_To_Location_When_Finds_Obstacle_At_Y_Minus_One() : void
+    {
+        $this->rover->location = array( 
+            'y' => $this->mars->obstacle['y'] +1 , 
+            'x' => $this->mars->obstacle['x']);
+
+        $this->rover->detectMarsObstacle( array(
+            'y' => $this->mars->obstacle['y'] , 
+            'x' => $this->mars->obstacle['x'] ));
+
+        $this->assertNotSame( $this->rover->location['y'], $this->mars->obstacle['y'] );
+
+
+    }
+
+
+    public function test_Rover_Confirm_Location_When_Does_Not_Find_Obstacles_At_Y_Plus_One() : void
+    {
+
+         $initialLocation = $this->rover->location;
+
+         $newLocation = array( 
+            'y' => $this->rover->location['y'] +1, 
+            'x' => $this->rover->location['x'] );
+
+        $this->rover->detectMarsObstacle( array(
+            'y' => $this->mars->obstacle['y'] , 
+            'x' => $this->mars->obstacle['x'] ));
+
+        $this->assertSame( $newLocation['y'], $initialLocation['y'] +1 );
+
+
+    }
+
+
+    public function test_Rover_Confirm_Location_When_Does_Not_Find_Obstacles_At_Y_Minus_One() : void
+    {
+
+         $initialLocation = $this->rover->location;
+
+         $newLocation = array( 
+            'y' => $this->rover->location['y'] -1, 
+            'x' => $this->rover->location['x']);
+
+        $this->rover->detectMarsObstacle( array(
+            'y' => $this->mars->obstacle['y'] , 
+            'x' => $this->mars->obstacle['x'] ));
+
+        $this->assertSame( $newLocation['y'], $initialLocation['y'] -1 );
+
+
+    }
+
 }
-
-
-
-

31fa937feeb1beca9b2f0c744e0bcfffdb925649
Newbiecodeweaver <carlosgilino@gmail.com>
2023-01-11 19:35:34 +0100

    remove mars constructor to use only default values

--- src/Mars.php
+++ src/Mars.php
@@ -10,25 +10,6 @@ require("../vendor/autoload.php");
     class Mars extends Planet {
 
         private $name = "Mars";
-        public $area = array();
-        public $obstacle = array();
-
-        public function __construct()
-        {
-            $this->area = array( 'x' => 25, 'y' => 25);
-            $this->obstacle = array( 'x' => 20, 'y' => 20 );
-        }
-
-        public function setArea( $x, $y )
-        {
-            $this->area = array( 'x' => $x, 'y' => $y);
-            return $this->name . " area is " . implode( "-", $this->area );
-        }
-
-        public function setObstacle( $x, $y )
-        {
-            $this->obstacle = array( 'x' => $x, 'y' => $y);
-            return $this->name . " obstacle is at " . implode( "-", $this->obstacle );
-        }
-
+        public $area =  array( 'x' => 25, 'y' => 25);
+        public $obstacle =  array( 'x' => 6, 'y' => 6);
     }
